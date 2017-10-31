b1f362d8d29d3813496735ff6babd54b595b9f37
Team Hwants <team.hwants@gmail.com>
2017-11-01 00:04:54 +1300

    Employee commit

--- src/nz/co/greg/paymentsystem/dto/Employee.java
+++ src/nz/co/greg/paymentsystem/dto/Employee.java
@@ -1,5 +1,35 @@
 package nz.co.greg.paymentsystem.dto;
 
-public class Employee {
+import java.io.Serializable;
 
+public class Employee implements Serializable {
+	private int employeeId;
+	private PaymentType paymentType;
+	public Employee(){}
+	public Employee(int employeeId, PaymentType paymentType) {
+		this.employeeId = employeeId;
+		this.paymentType = paymentType;
+	}
+
+	public PaymentType getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(PaymentType paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}	
+	
+	public int calculatePayment(int hours)
+	{
+		try {
+			return paymentType.calculatePayment(hours);
+		} catch (Exception any) {
+			System.err.println("ERROR!! "+any.getStackTrace());
+			return 0;
+		}
+	}
 }
