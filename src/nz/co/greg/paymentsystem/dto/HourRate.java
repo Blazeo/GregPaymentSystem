dcbaf6337d73f5da9c07c1bad097ca641c64e624
Blazeo <greg.thomas90.gt@gmail.com>
2017-10-31 23:44:41 +1300

    ok ok sir i love you sir

---
+++ src/nz/co/greg/paymentsystem/dto/HourRate.java
@@ -0,0 +1,5 @@
+package nz.co.greg.paymentsystem.dto;
+
+public class HourRate extends PaymentType {
+
+}
---
+++ src/nz/co/greg/paymentsystem/dto/PaymentType.java
@@ -0,0 +1,10 @@
+package nz.co.greg.paymentsystem.dto;
+
+public class PaymentType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
